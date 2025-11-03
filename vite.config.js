525694f31064ed80623a271c093f33727952691a
Eliot Dupouy <35254392+eliotdupouy@users.noreply.github.com>
2025-11-03 13:55:21 +0100

    Delete vite.config.js

--- vite.config.js
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// When deploying to GitHub Pages for username.github.io/repo, set base to /repo/
-export default defineConfig({
-  base: '/gamedev/',
-  plugins: [react()]
-})
